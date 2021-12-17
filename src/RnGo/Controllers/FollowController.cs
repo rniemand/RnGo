7e149ea0c7b8f54fd497e0de48a3cd09476390b5
Richard Niemand <niemand.richard@gmail.com>
2021-12-17 14:55:59 -0700

    Adding follow controller

---
+++ src/RnGo/Controllers/FollowController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RnGo.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class FollowController : ControllerBase
+  {
+    [HttpGet, Route("{id}")]
+    public ActionResult Get(string id)
+    {
+      return Redirect($"https://google.ca?q={id}");
+    }
+  }
+}
