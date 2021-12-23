12916c941d1b856c86e0985fe854d4772b0e5051
Richard Niemand <niemand.richard@gmail.com>
2021-12-23 14:55:35 -0700

    Added AddLinkResponse

---
+++ src/RnGo.Core/Models/Responses/AddLinkResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace RnGo.Core.Models.Responses
+{
+  public class AddLinkResponse
+  {
+    [JsonProperty("success"), JsonPropertyName("success")]
+    public bool Success { get; set; }
+
+    [JsonProperty("messages"), JsonPropertyName("messages")]
+    public string[] Messages { get; set; }
+
+    [JsonProperty("shortCode"), JsonPropertyName("shortCode")]
+    public string ShortCode { get; set; }
+
+    public AddLinkResponse()
+    {
+      // TODO: [AddLinkResponse] (TESTS) Add tests
+      Success = false;
+      Messages = Array.Empty<string>();
+      ShortCode = string.Empty;
+    }
+  }
+}
