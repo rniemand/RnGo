b2285d4e60dba1cbc3ff8c8e07a2d5accdf35c19
Richard Niemand <niemand.richard@gmail.com>
2022-05-02 21:15:55 -0600

    Patching build

---
+++ test/RnGo.Core.T1.Tests/Services/ApiKeyServiceTests/RefreshApiKeysTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using RnGo.Core.Entities;
+using RnGo.Core.Providers;
+using RnGo.Core.Repos;
+using RnGo.Core.T1.Tests.TestSupport.Builders;
+
+namespace RnGo.Core.T1.Tests.Services.ApiKeyServiceTests;
+
+[TestFixture]
+public class RefreshApiKeysTests
+{
+  [Test]
+  public void RefreshApiKeys_GivenCalled_ShouldClearApiKeys()
+  {
+    // Arrange
+    var configProvider = Substitute.For<IRnGoConfigProvider>();
+    var apiKeyRepo = Substitute.For<IApiKeyRepo>();
+
+    configProvider.Provide().Returns(RnGoConfigBuilder.ValidDefault);
+    apiKeyRepo.GetEnabledApiKeys().Returns(
+      new List<ApiKeyEntity> { new ApiKeyEntityBuilder().BuildWithValidDefaults() },
+      new List<ApiKeyEntity>()
+    );
+
+    var service = TestHelper.GetApiKeyService(
+      configProvider: configProvider,
+      apiKeyRepo: apiKeyRepo
+    );
+
+    // Act
+    Assert.That(service.ApiKeys.Count, Is.EqualTo(2));
+    service.RefreshApiKeys();
+
+    // Assert
+    Assert.That(service.ApiKeys.Count, Is.EqualTo(1));
+  }
+}
---
+++ test/RnGo.Core.T1.Tests/Services/ApiKeyServiceTests/TestHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using RnGo.Core.Entities;
+using RnGo.Core.Providers;
+using RnGo.Core.Repos;
+using RnGo.Core.Services;
+using RnGo.Core.T1.Tests.TestSupport.Builders;
+
+namespace RnGo.Core.T1.Tests.Services.ApiKeyServiceTests;
+
+public class TestHelper
+{
+  public static ApiKeyService GetApiKeyService(
+    ILogger<ApiKeyService>? logger = null,
+    IRnGoConfigProvider? configProvider = null,
+    IApiKeyRepo? apiKeyRepo = null)
+  {
+    return new ApiKeyService(
+      logger ?? Substitute.For<ILogger<ApiKeyService>>(),
+      EnsureValidConfigProvider(configProvider),
+      EnsureValidApiKeyRepo(apiKeyRepo)
+    );
+  }
+
+  private static IRnGoConfigProvider EnsureValidConfigProvider(IRnGoConfigProvider? provider = null)
+  {
+    if (provider is not null)
+      return provider;
+
+    provider = Substitute.For<IRnGoConfigProvider>();
+    provider.Provide().Returns(RnGoConfigBuilder.ValidDefault);
+    return provider;
+  }
+
+  private static IApiKeyRepo EnsureValidApiKeyRepo(IApiKeyRepo? repo = null)
+  {
+    if (repo is not null)
+      return repo;
+
+    repo = Substitute.For<IApiKeyRepo>();
+    repo.GetEnabledApiKeys().Returns(new List<ApiKeyEntity>());
+    return repo;
+  }
+}
