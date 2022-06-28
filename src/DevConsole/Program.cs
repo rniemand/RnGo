1816fe5be17413d4aa4befdabdc700730f089a89
Richard Niemand <niemand.richard@gmail.com>
2022-06-28 11:18:15 -0600

    Setting default connection

--- src/DevConsole/Program.cs
+++ src/DevConsole/Program.cs
@@ -1,9 +1,10 @@
 using DevConsole;
 
-var linkUrl = await RnGoDevelopment.ResolveLink("a");
-var encoded = RnGoDevelopment.Base64Encode("Hello");
-var generatedLink = RnGoDevelopment.GenerateLinkString(121);
-var addLinkResponse = await RnGoDevelopment.AddLink("http://google.ca");
-await RnGoDevelopment.AddDatabaseLink();
-var linkCount = await RnGoDevelopment.GetLinkCount();
-var apiKeyEntity = await RnGoDevelopment.StoreApiKey("92087E4B-4913-4005-8171-3F38E464797E");
+//var linkUrl = await RnGoDevelopment.ResolveLink("a");
+//var encoded = RnGoDevelopment.Base64Encode("Hello");
+//var generatedLink = RnGoDevelopment.GenerateLinkString(121);
+//var addLinkResponse = await RnGoDevelopment.AddLink("http://google.ca");
+//await RnGoDevelopment.AddDatabaseLink();
+//var linkCount = await RnGoDevelopment.GetLinkCount();
+//var apiKeyEntity = await RnGoDevelopment.StoreApiKey("92087E4B-4913-4005-8171-3F38E464797E");
+
